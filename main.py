c0eb0108a162011a727ad986d292b723c594cdf5
ictu-dungnt98 <ntdungembedded98@gmail.com>
2024-03-18 21:54:04 +0700

    close and open app success

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 from ppadb.client import Client as AdbClient
 import subprocess
+import time
 
+# list cac buoc
+LOGIN = 1
+CLICK_NHAN_VAT = LOGIN + 1
+CLICK_QUANG_CAO = CLICK_NHAN_VAT + 1
+
+def get_screen_dimensions():
+    # Run adb command to get the screen dimensions
+    adb_cmd = "adb shell wm size"
+    result = subprocess.run(adb_cmd, shell=True, capture_output=True, text=True)
+    output = result.stdout.strip()
+    # Extract screen width and height from the output
+    width, height = map(int, output.split()[-1].split('x'))
+    return width, height
+
+def click_center_of_screen():
+    # Get screen dimensions
+    width, height = get_screen_dimensions()
+    # Calculate center coordinates
+    center_x = width // 2
+    center_y = height // 2
+    # Run adb command to send a tap event at the center of the screen
+    adb_cmd = f"adb shell input tap {center_x} {center_y}"
+    print(adb_cmd)
+    subprocess.run(adb_cmd, shell=True)
 
 def open_app(device, package_name, activity_name):
     # Command to open the app
     command = f"am start -n {package_name}/{activity_name}"
+    device.shell(command)
+
+def close_app(package_name):
+    adb_cmd = f"adb shell am force-stop {package_name}"
+    subprocess.run(adb_cmd, shell=True)
+
+def click_to_position(device, x, y):
+    command = f"input tap {x} {y}"
     device.shell(command)
 
 def connect():
@@ -22,12 +55,22 @@ def connect():
 def main():
     device, client = connect()
 
-    # Package name of the app you want to open
+    step = LOGIN
     package_name = "com.playmini.miniworld"
-    # Activity name of the app you want to open
     activity_name = "org.appplay.lib.AppPlayBaseActivity"
     
-    open_app(device, package_name, activity_name)
+    # close app
+    close_app(package_name)
+    time.sleep(1)
+
+    while True:
+        if step == LOGIN:
+            open_app(device, package_name, activity_name)
+            step = CLICK_NHAN_VAT
+        if step == CLICK_NHAN_VAT:
+            click_center_of_screen()
+        if step == CLICK_QUANG_CAO:
+            pass
 
 if __name__ == "__main__":
     main()
