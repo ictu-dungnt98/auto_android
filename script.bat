b5a1e14e1d4db62abb8e906cfaf78d99ace91a7a
ictu-dungnt98 <ntdungembedded98@gmail.com>
2024-03-29 00:39:06 +0700

    add script capture all screen

--- script.bat
+++ script.bat
@@ -1,2 +1,12 @@
-adb -s %1 shell screencap /sdcard/pic.png
-adb -s %1 pull /sdcard/pic.png
+set "dataFile=device_id.txt"
+
+set "list="
+
+for /f "usebackq delims=" %%i in ("%dataFile%") do (
+    REM Append each line to the list variable
+    adb -s "%%i" shell screencap "/sdcard/%%i.png"
+    adb -s "%%i" pull /sdcard/%%i.png
+)
+
+@REM adb -s %1 shell screencap /sdcard/%1.png
+@REM adb -s %1 pull /sdcard/%1.png
